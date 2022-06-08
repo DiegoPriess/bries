02044e76dbc825ceacca715ffd5350c73b51b0ce
Diego Priess <diegopriess.dev@gmail.com>
2022-06-07 21:14:07 -0300

    First commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) throws ClassNotFoundException {
+        String a = "ÉUQ";
+        System.out.println("Eu sou o seu primeiro programa.");
+        DataBase.connect();
+    }
+}
